0f3eba8426ad8fdeeac6f86b6eb54160e29b935b
Alban <Alban@TAICHI_ALBAN>
2014-08-15 12:04:02 +0200

    1er commit de ce fichier

---
+++ src/gittestlumiere/GitTestLumiere.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gittestlumiere;
+
+/**
+ *
+ * @author Alban
+ */
+public class GitTestLumiere {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("1.Hello World"); 
+                
+        System.out.println("2.Good Morning");
+        
+        System.out.println("3.Lumière!");
+        
+        
+        
+        
+        
+        
+        
+        /*
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        */
+    }
+    
+}
