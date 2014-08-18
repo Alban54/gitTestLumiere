552fbabe5aed7ece726b2072f9dabfeeb6e67c52
Alban54 <albanbeaujoie@gmail.com>
2014-08-18 10:52:38 +0200

    version stable

--- src/gittestlumiere/GitTestLumiere.java
+++ src/gittestlumiere/GitTestLumiere.java
@@ -45,6 +45,11 @@ public class GitTestLumiere {
         
         /*
         
+        //C'est bon apres un pull et un merge inclut obligatoire et un commit c'est redevenu
+        // possible de faire des commit remote.
+        
+       
+        
         
         
         
